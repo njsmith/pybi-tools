017694dd7a2277fd45ff9ad8b96ec91d6eee05c9
Nathaniel J. Smith <njs@pobox.com>
2022-10-24 00:35:47 -0700

    clarify note-to-self

--- do-manylinux.py
+++ do-manylinux.py
@@ -1,5 +1,7 @@
-# (cd manylinux; POLICY=manylinux2014 PLATFORM=x86_64 ./build.sh ./build.sh)
-# for VERSION in $(seq -f 3.9.%g 6) $(seq -f 3.8.%g 11) $(seq -f 3.7.%g 11) $(seq -f 3.6.%g 14); do docker run --rm -it -v $(pwd):/host pybi-build-image sh -c "PREFIX=/pyinstall /build_scripts/build-cpython.sh ${VERSION} && /opt/_internal/cpython-3.9.5/bin/python3 -m ensurepip && PYTHONPATH=/host/local-pkgs /opt/_internal/cpython-3.9.5/bin/python3 /host/do-manylinux.py"; done
+# to build local pybi-build-image:
+#   (cd ~/manylinux; POLICY=manylinux2014 PLATFORM=x86_64 ./build.sh)
+# to use it to build pybis:
+#   for VERSION in $(seq -f 3.9.%g 6) $(seq -f 3.8.%g 11) $(seq -f 3.7.%g 11) $(seq -f 3.6.%g 14); do docker run --rm -it -v $(pwd):/host pybi-build-image sh -c "PREFIX=/pyinstall /build_scripts/build-cpython.sh ${VERSION} && /opt/_internal/cpython-3.9.5/bin/python3 -m ensurepip && PYTHONPATH=/host/local-pkgs /opt/_internal/cpython-3.9.5/bin/python3 /host/do-manylinux.py"; done
 
 import sys
 from pathlib import Path
